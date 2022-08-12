3d6da02b73f9f9921382a3e69383035e8cb6029c
Abdurro'uf <abdurrouf@asiaa.sinica.edu.tw>
2022-08-12 09:26:09 +0700

    Create download.sh

---
+++ ERO_SMACS0723/data/download.sh
@@ -0,0 +1,27 @@
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f435w_drc_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f435w_drc_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f606w_drc_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f606w_drc_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f814w_drc_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f814w_drc_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f105w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f105w_drz_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f125w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f125w_drz_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f140w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f140w_drz_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f160w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f160w_drz_wht.fits
+
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f090w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f090w_drz_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f150w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f150w_drz_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f200w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f200w_drz_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f277w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f277w_drz_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f356w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f356w_drz_wht.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f444w_drz_sci.fits
+wget https://s3.amazonaws.com/grizli-v2/SMACS0723/Test/smacs0723-f444w_drz_wht.fits
